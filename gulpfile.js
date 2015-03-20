71c9bb249cd82c1f5a139321cb8c2a468987a9ff
Liam Duffy <liam.p.duffy@gmail.com>
2015-03-20 14:35:11 +0000

    Nicer error handling. Gerrid if functionality is added to gulp to let you do this with its own config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var browserSync = require('browser-sync'),
     reload = browserSync.reload,
+    colors = require('colors'),
     cp = require('child_process'),
     gulp = require('gulp'),
     minifyHtml = require('gulp-minify-html'),
@@ -8,51 +9,74 @@ var browserSync = require('browser-sync'),
     svgstore = require('gulp-svgstore'),
     uglify = require('gulp-uglify');
 
+// error reporting to console;
+var logError = function(error) {
+    var message =
+        '\n--------------------\n' +
+        'Plugin ' + error.plugin + ' failed:' +
+        '\nError at ' + error.fileName + ':' + error.lineNumber +
+        '\n' + error.message +
+        '\n--------------------\n';
+
+    console.error(message.red);
+}
+
 gulp.task('jekyll', function(done) {
     return cp
         .spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', function() {
-            gulp.src('jekyll/_site/**/*.html')
-                .pipe(minifyHtml())
-                .pipe(gulp.dest('public'));
+        .on('close', function(code) {
+            if(code === 0) {
+                gulp.src('jekyll/_site/**/*.html')
+                    .pipe(minifyHtml())
+                    .pipe(gulp.dest('public'));
 
-            reload();
+                reload();
+            }
+
+            // jekyll outputs to console anyway, so no error handling required
+
             done();
         });
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function(done) {
     return gulp
         .src('sass/*.scss')
         .pipe(sass({
             includePaths: require('node-bourbon').includePaths,
             outputStyle: 'compressed'
         }))
-        .pipe(gulp.dest('public/css'))
-        .pipe(reload({ stream: true }));
+        .on('error', function(error) {
+            logError(error);
+            done();
+        })
+        .pipe(reload({ stream: true }))
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('svgstore', function () {
-    var stream = gulp
+gulp.task('svgstore', function() {
+    return gulp
         .src('icons/*.svg')
         .pipe(svgmin())
         .pipe(svgstore())
+        .on('error', function(error) {
+            logError(error);
+            done();
+        })
+        .on('end', reload)
         .pipe(gulp.dest('public/svg'));
-
-    stream.on('end', reload);
-
-    return stream;
 });
 
-gulp.task('uglify', function () {
-    var stream = gulp
+gulp.task('uglify', function(done) {
+    return gulp
         .src('js/*.js')
         .pipe(uglify())
+        .on('error', function(error) {
+            logError(error);
+            done();
+        })
+        .on('end', reload)
         .pipe(gulp.dest('public/js'));
-
-    stream.on('end', reload);
-
-    return stream;
 });
 
 gulp.task('default', ['jekyll', 'sass', 'svgstore', 'uglify']);
