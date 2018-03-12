f43666b1aebeed31f434069f05003d8b01d3488d
Liam Duffy <3338808+liamness@users.noreply.github.com>
2018-03-12 02:26:46 +0000

    Containerized development workflow

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-rm -rf public/posts
-rm -f public/svg/*.svg
-rm -f public/*.js
-rm -f public/*.css
-find public -name *.html ! -name google*.html -delete
+# Run in container to get around permissions issues
+docker-compose run node-service bash -c "
+  rm -rf public/posts
+  rm -f public/svg/*.svg
+  rm -f public/*.js
+  rm -f public/*.css
+  find public -name *.html ! -name google*.html -delete
+"
